99ed2dedd088c26f1ea1b4aacc980b7e0da4a794
harlahsaduki <163113239+harlahsaduki@users.noreply.github.com>
2024-04-10 00:26:05 +0700

    Update page.tsx

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -91,7 +91,15 @@ const jsonLd = {
         embedUrl: `https://${upstream}/e/${file.filecode}`,
         uploadDate: new Date(
             file.uploaded + ".000Z"
-        ).toISOString()
+        ).toISOString(),
+        interactionStatistic: {
+            '@type': `InteractionCounter`,
+                userInteractionCount: `${file.views}`,
+            interactionType: {
+                '@type': `WatchAction`,
+                target: `https://bokepbugil.pages.dev/v/${file.filecode}`
+            }  
+        }
         }
         const jsonLd2 = {
         '@context': 'https://schema.org',
@@ -111,7 +119,26 @@ const jsonLd = {
                 '@type': 'Person',
                 name: 'admin',
                 url: 'https://bokepbugil.pages.dev'
-              }
+              },
+        interactionStatistic: {
+            '@type': `InteractionCounter`,
+                userInteractionCount: `${file.views}`,
+            interactionType: {
+                '@type': `ReadAction`,
+                target: `https://bokepbugil.pages.dev/v/${file.filecode}`
+            }  
+        }
+        }
+        const jsonLd3 = {
+            '@context': 'https://schema.org', 
+            '@type': 'Book', 
+            'name': `${file.title} - ${SITENAME}`, 
+            'aggregateRating': {
+            '@type': 'AggregateRating',	
+                'ratingValue': '5',	
+                'ratingCount': `${file.views}`,	
+                'bestRating': '5',	
+                'worstRating': '1' }
         }
     return (
         <div className="grid col-span-full gap-4 md:gap-4 md:mx-10">
@@ -125,11 +152,15 @@ const jsonLd = {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd2) }}
         />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd3) }}
+        />
         {/* ... */}
         </section>
             <iframe
                 className="w-full h-[30vh] md:h-[55vh] lg:h-[70vh]"
-                src={`https://${upstream}${file.protected_embed}`}
+                src={`https://${upstream}/e/${file.filecode}`}
                 scrolling="no"
                 title={file.title}
                 frameBorder={0}
